57a2545483b29e6b655399d7597ca70437876617
SJKim <worldsjk1990@gmail.com>
2021-03-25 11:50:43 +0900

    chat with socketIO

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -14,4 +16,17 @@ const handleListening = () =>
 
 const server = app.listen(PORT, handleListening);
 
+// socket은 http와 달리 페이지가 없고 연결만 있음. io가 모든 이벤트를 감지해야 함.
 const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,21 @@
-hello;
+// client에서 io를 연결시키고 이벤트를 항상 듣고 있어야 함.
+// 서버에서 emit / broadcast 이벤트를 방출했을 때 듣고 반응함.
+// client, server 모두 이벤트를 듣고 발생시킬 수 있음.
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`You: ${message}`);
+}
+
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
