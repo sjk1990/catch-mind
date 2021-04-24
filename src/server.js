18c0715ff30e0f147bf85c6e3f122129be022fed
SJKim <worldsjk1990@gmail.com>
2021-04-24 23:30:30 +0900

    login flow & set global variable(socket)

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
 import logger from "morgan";
+import socketController from "./socketController";
+import events from "./events";
 
 const PORT = 4000;
 const app = express();
@@ -9,7 +11,9 @@ app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) =>
+  res.render("home", { events: JSON.stringify(events) })
+);
 
 const handleListening = () =>
   console.log(`✅ Server running: http://localhost:${PORT}`);
@@ -19,14 +23,4 @@ const server = app.listen(PORT, handleListening);
 // socket은 http와 달리 페이지가 없고 연결만 있음. io가 모든 이벤트를 감지해야 함.
 const io = socketIO(server);
 
-io.on("connection", (socket) => {
-  socket.on("newMessage", ({ message }) => {
-    socket.broadcast.emit("messageNotif", {
-      message,
-      nickname: socket.nickname || "Anon",
-    });
-  });
-  socket.on("setNickname", ({ nickname }) => {
-    socket.nickname = nickname;
-  });
-});
+io.on("connection", (socket) => socketController(socket));
